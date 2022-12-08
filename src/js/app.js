3fc32a2ddfcffd0894477f292bd92f9036c51fbf
petrborr <petrborr@gmail.com>
2022-12-08 15:21:34 +0200

    add: image that scales on click

--- src/js/app.js
+++ src/js/app.js
@@ -3,9 +3,8 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
-  const button = document.querySelector(".button");
-  button.addEventListener("click", () => {
-    const image = document.querySelector("div.image")
+  const image = document.querySelector(".image");
+  image.addEventListener("click", () => {
     image.style.transform = "scale(2)"
     alert("💣");
   });
